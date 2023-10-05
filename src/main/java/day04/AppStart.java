a59acab042ae358c189f27d6941fc55710c001e4
504-t <itdanja@kakao.com>
2023-10-05 14:07:37 +0900

    Todo Spring 정적파일(html,css,js) 파일 반환하는 방법

--- src/main/java/day04/AppStart.java
+++ src/main/java/day04/AppStart.java
@@ -10,3 +10,18 @@ public class AppStart {
         SpringApplication.run( AppStart.class );
     }
 }
+
+/*
+    - 정적 파일 생성 위치
+    스프링이 정적(view) 파일들을 찾는 위치 resources폴더
+        - 주의할점 : 본인이 만들고 싶은곳에 정적(view) 파일 만들면 안된다.
+    HTML : resources->templates-> html파일
+    JS/CSS/Image : resources->static-> JS/CSS/Image파일
+
+    - JSP프로젝트 와 SPRING 프로젝트의 정적파일 경로 차이
+        - JSP는 패키지의 경로와 파일명이 곧 URL
+            http://localhost/Ezen_teamB/jsp/index.jsp
+        - SPRING 는 정적파일 호출하는 URL 매핑
+            매핑후 Resource 타입 반환
+
+ */
--- src/main/java/day04/TodoController.java
+++ src/main/java/day04/TodoController.java
@@ -1,6 +1,8 @@
 package day04;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,6 +47,14 @@ public class TodoController {
         System.out.println("TodoController.doDelete");   System.out.println("tno = " + tno);
         return todoService.doDelete( tno );
     }
+
+    // 5. HTML 반환 매핑 주소만들기
+    @GetMapping("/index") // http://localhost/todo/index
+    public Resource getIndex(){
+        return new ClassPathResource("templates/todo.html");
+    }
+        // Resource : import org.springframework.core.io.Resource;
+        // ClassPathResource : import org.springframework.core.io.ClassPathResource;
 }
 
 
