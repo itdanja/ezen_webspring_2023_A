7fafc2309ea29072c1601c31a99d16b1909d8c53
ãê°강사 <itdanja@kakao.com>
2023-10-12 17:20:56 +0900

    Todo CRUD JPA

---
+++ src/main/java/example/day03/mapping/ParamDto.java
@@ -0,0 +1,13 @@
+package example.day03.mapping;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ParamDto {
+    private String param1;
+    private int param2;
+}
--- src/main/resources/static/todo.js
+++ src/main/resources/static/todo.js
@@ -1,1 +1,9 @@
 console.log('todo.js open')
+
+// 1. POST AJAX
+
+// 2. GET AJAX
+
+// 3. PUT  AJAX
+
+// 4. DELETE  AJAX
