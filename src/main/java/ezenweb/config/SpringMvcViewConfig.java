78a028a9e2533352fee53543afa8c2c73de99ee6
새ë¡itdanja <itdanja@kakao.com>
2023-10-25 15:26:05 +0900

    day95 헤더에서 로그인상태 호출하기

--- src/main/java/ezenweb/config/SpringMvcViewConfig.java
+++ src/main/java/ezenweb/config/SpringMvcViewConfig.java
@@ -8,13 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class SpringMvcViewConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        //super.addViewControllers(registry); // 기존의 view 아키텍처 사용 안함
+        //super.addViewControllers(registry);
         registry.addViewController("/{spring:\\w+}").setViewName("forward:/");
         registry.addViewController("/**/{spring:\\w+}").setViewName("forward:/");
-        // spring 2.6 이상 [ 해당 패턴 경로 못찾음.. ]
-        // 해결 방안 : application.properties[ spring.mvc.pathmatch.matching-strategy = ant_path_matcher ]
-        registry.addViewController("/{spring:\\w+}/**/**/**/**/**/**{spring:?!(\\.js|\\.css)$}").setViewName("forward:/");
-                    //      /**/**/**/**/**/**{spring:?!(\.js|\.css)
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}").setViewName("forward:/");
     }
 }
 /*
