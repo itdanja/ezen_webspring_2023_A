e1eb89258607127706e3fd752aae358f818bac86
새ë¡itdanja <itdanja@kakao.com>
2023-11-06 14:39:05 +0900

    day102 개별게시물출력

--- src/main/java/ezenweb/controller/BoardController.java
+++ src/main/java/ezenweb/controller/BoardController.java
@@ -12,22 +12,28 @@ import java.util.List;
 public class BoardController {
     @Autowired
     private BoardService boardService;
-    // 1.
+    // 1. 게시물 등록
     @PostMapping("")
     public boolean write( BoardDto boardDto ){
         return boardService.write( boardDto );
     }
-    // 2.
+    // 2. 전체게시물출력
     @GetMapping("")
     public List<BoardDto> getAll(){
         return boardService.getAll();
     }
-    // 3.
+    // 2-2 . 개별게시물출력
+    @GetMapping("/doGet")
+    public BoardDto doGet( @RequestParam int bno ){
+        return boardService.doGet( bno );
+    }
+
+    // 3. 게시물 수정
     @PutMapping("")
     public boolean update( BoardDto boardDto ){
         return boardService.update( boardDto );
     }
-    // 4
+    // 4 게시물삭제
     @DeleteMapping("") // http://localhost:80/board?bno=1
     public boolean delete( @RequestParam int bno){
         return boardService.delete( bno );
--- src/main/reactapp/src/component/board/BoardView.js
+++ src/main/reactapp/src/component/board/BoardView.js
@@ -1,12 +1,23 @@
 import { useSearchParams , Link } from 'react-router-dom'
 import axios from 'axios'
+import { useState , useEffect } from 'react'
 
 export default function BoardView( props ){
-
+    // 1. HTTP 경로상의 쿼리스트링 매개변수 호출
     const [ searchParams, setSearchParams ] = useSearchParams();
     const bno = searchParams.get('bno');
 
-    // 1. 삭제 axios
+    // 2. 현재 게시물의 정보를 가지는 상태관리 변수
+    const [ board , setBoard ] = useState( { } )
+
+    // 3. 개별 게시물 axios [ 실행조건 : 컴포넌트 최초 1번 실행 ]
+    const onGet = (e) => {
+        axios.get( '/board/doGet' , { params : { bno : bno } }  )
+            .then( r => { setBoard(r.data); })
+    }
+    useEffect( ()=>{ onGet() } , [] )
+
+    // 4. 삭제 axios [ 실행조건 : 삭제버튼 클릭했을떄 ]
     const onDelete = (e) =>{
         axios.delete( '/board' , { params : { bno : bno } } )
             .then( r =>{
@@ -17,9 +28,13 @@ export default function BoardView( props ){
                 else{ alert('게시물 삭제 실패')}
             })
     }
+
     return(<>
         <div>
             <h3> 개별 게시물 { bno } </h3>
+            <div>{ board.btitle }</div>
+            <div>{ board.bcontent }</div>
+
             <button type="button" onClick={ onDelete } > 삭제 </button>
             <Link to={'/board/update?bno='+bno}>
                 <button type="button"> 수정 </button>
