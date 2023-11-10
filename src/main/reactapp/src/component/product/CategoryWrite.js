cbb8d4f1f5e3a925824b4d34ea6f5f4a3fc8948b
새ë¡itdanja <itdanja@kakao.com>
2023-11-10 12:25:16 +0900

    day106 카테고리 등록/출력 AXIOS

--- src/main/reactapp/src/component/product/CategoryWrite.js
+++ src/main/reactapp/src/component/product/CategoryWrite.js
@@ -1,5 +1,51 @@
+
+import axios from 'axios';
+import { useEffect , useState } from 'react'
 export default function CategoryWrite( props ){
+
+    // 0.출력할 카테고리 목록 을 저장하는 상태변수
+    const [ categoryList , setCategoryList ] = useState( [] );
+
+    // 1. 카테고리 등록 AXIOS // 등록버튼을 클릭했을때
+    const addCategory = (e)=>{
+        const info = { pcname : document.querySelector('.pcname').value }
+        axios.post( '/product/category' , info )
+            .then( r => {
+                if( r.data ){alert("카테고리등록성공"); printCategory() }
+                else{ alert("카테고리등록실패")}
+             } )
+    }
+    // 2. 카테고리 출력 AXIOS // 컴포넌트가 열렸을때 / 등록되었을때[재랜더링]
+    const printCategory = (e)=>{
+        axios.get('/product/category')
+            .then( r => { console.log(r.data); setCategoryList(r.data); } );
+    }
+    useEffect( ()=>{ printCategory() } , [ ] )
+
+    // 3. 카테고리 수정 AXIOS // 수정버튼을 클릭했을때
+    const updateCategory = (e , pcno )=>{
+        const info = { pcno : pcno   , pcname : "" }
+        axios.put('/product/category' , info )
+            .then( r => { console.log(r.data); } );
+    }
+    // 4. 카테고리 삭제 AXIOS // 삭제버튼을 클릭했을때
+    const deleteCategory = ( e , pcno )=>{
+        axios.delete('/product/category' , { params : { pcno : pcno } }  )
+            .then( r=>{ console.log(r.data); })
+    }
+
     return(<>
-        <h3> 카테고리 등록 </h3>
+        <div style={{ width : '300px' , margin : '0 auto' }}>
+            <h3> 카테고리 등록 </h3>
+            <form>
+                <input type="text"
+                    className="pcname"
+                    placeholder="등록할카테고리명" />
+
+                <button type="button"
+                    onClick={ addCategory }>등록</button>
+            </form>
+        </div>
     </>)
+
 }
--- src/main/reactapp/src/component/product/ProductAdmin.js
+++ src/main/reactapp/src/component/product/ProductAdmin.js
@@ -6,27 +6,39 @@ import TabContext from '@mui/lab/TabContext'; // npm i @mui/lab 설치 필수
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 
+import CategoryWrite from './CategoryWrite'
+import ProductInfo from './ProductInfo'
+import ProductList from './ProductList'
+import ProductWrite from './ProductWrite'
+
 export default function ProductAdmin( props ){
     // 2.
     const [value, setValue] = React.useState('1');
     // 3.
     const handleChange = (event, newValue) => { setValue(newValue); };
-
     return(<>
-         <h3> 제품 관리 페이지 </h3>
          {/*4.*/}
          <Box sx={{ width: '100%', typography: 'body1' }}>
            <TabContext value={value}>
              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-               <TabList onChange={handleChange} aria-label="lab API tabs example">
-                 <Tab label="Item One" value="1" />
-                 <Tab label="Item Two" value="2" />
-                 <Tab label="Item Three" value="3" />
+               {/* 탭 에 포함된 제목들 */}
+               <TabList
+                    onChange={handleChange}
+                    aria-label="lab API tabs example"
+                    indicatorColor="secondary"
+                    centered
+                    >
+                 <Tab label="카테고리 등록" value="1" /> { /* value : 탭순서번호(식별) */}
+                 <Tab label="제품 등록" value="2" />
+                 <Tab label="제품 목록" value="3" />
+                 <Tab label="제품 상태" value="4" />
                </TabList>
              </Box>
-             <TabPanel value="1">Item One</TabPanel>
-             <TabPanel value="2">Item Two</TabPanel>
-             <TabPanel value="3">Item Three</TabPanel>
+              {/* 탭 선택시 출력되는 내용물 */}
+             <TabPanel value="1"> <CategoryWrite /> </TabPanel>
+             <TabPanel value="2"> <ProductWrite /> </TabPanel>
+             <TabPanel value="3"> <ProductList /> </TabPanel>
+             <TabPanel value="4"> <ProductInfo /> </TabPanel>
            </TabContext>
          </Box>
     </>)
